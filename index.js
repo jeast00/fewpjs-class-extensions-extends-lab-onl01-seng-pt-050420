47e61152ba547a3114bb3b777dfeb3a118885d5a
Jonathan East <jonathan_east@ymail.com>
2020-12-13 12:26:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ class Triangle extends Polygon {
     let side2 = this.sides[1];
     let side3 = this.sides[2];
     
-    return ((side1 + side2 > side3) && ())
+    return ((side1 + side2 > side3) && (side1 + side3 > side 2) && (side2 + side3 > side1));
   }
 }
